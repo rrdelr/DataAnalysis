03855a1921114d4c7f44a3a0705539994e52e4d8
rrdelr <roquer1945@gmail.com>
2021-06-04 10:01:02 -0500

    Add files via upload

--- funcs.py
+++ funcs.py
@@ -3,31 +3,37 @@
 # TBD - Finish labeling data
 # TBD - Comment more sections of code for clarity
 
-# Imports
+# Import
+import os
+import plotly
 
 # Import as
 import pandas as pd
 import plotly.graph_objs as go
 import numpy as np
 import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Import from
 from plotly.subplots import make_subplots
 from scipy import stats
+from sklearn.linear_model import LinearRegression
 
+plotly.io.orca.config.executable = 'C:/Users/RRX\AppData\Local\Programs\orca/orca.exe'
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # PATH variables (Main path and subfolders)
 PATH = "./Data/"
+PATHX = "./Output/"
 # Plotting functions
 
-def to_df(stent):
-    df = pd.read_excel(PATH + stent, parse_dates=['Date'])
-    # print(df.head().to_string())
-    return df[0:98241]
+def to_df(stent, sheet):
+    df = pd.read_excel(PATH + stent, parse_dates=['Date'], nrows=98241, sheet_name=sheet)
+    return df
 
 
-def gen_scat(df, stmo, endmo, vart, *argv):
+def gen_scat(df, name, stmo, endmo, vart, *argv):
     """ Generic scatter plot
 
     Arguments:
@@ -43,7 +49,7 @@ def gen_scat(df, stmo, endmo, vart, *argv):
 
     df[vart] = pd.to_datetime(df['Date']).dt.date
     df[vart] = df[vart].astype(str) + ' ' + df['UTC Time'].astype(str)
-    df[vart] = df[vart][0:98241]
+    # df[vart] = df[vart][0:98241]
     df[vart] = pd.to_datetime(df[vart])
 
     # date adjustment
@@ -60,13 +66,40 @@ def gen_scat(df, stmo, endmo, vart, *argv):
             row=1,
             col=1
         )
+        rolling = df[arg].rolling(1080).mean()
+        fig.append_trace(go.Scatter(
+            x=df[vart],
+            y=rolling,
+            name=arg+"(P. 1D)"),
+            row=1,
+            col=1
+        )
+        # rolling = df[arg].rolling(1080*3).mean()
+        # fig.append_trace(go.Scatter(
+        #     x=df[vart],
+        #     y=rolling,
+        #     name=arg + "(3da)"),
+        #     row=1,
+        #     col=1
+        # )
+        rolling = df[arg].rolling(1080*6).mean()
+        fig.append_trace(go.Scatter(
+            x=df[vart],
+            y=rolling,
+            name=arg + "(P. 6D)"),
+            row=1,
+            col=1
+        )
 
     fig.update_layout(
         title_text=(" "),
         title_x=0.5,
         font=dict(size=18))
 
-    fig.show()
+    # fig.show()
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    fig.write_image(PATHX + name + "/avg_plot" + argv[0] + ".png", width=1250, height=750)
 
 
 def df_tf(df, vart, d1, m1, y1, d2, m2, y2):
@@ -147,7 +180,7 @@ def num2range(d, rangestx, ranges, index=False):
         return rangestx[lx]
 
 
-def wrose(dfa, speed, dir, n_bins):
+def wrose(dfa, name, speed, dir, n_bins):
     df = dfa.filter([speed, dir],axis=1)
     df = flipdir(df, dir)
     # df = DataF.dropna()
@@ -168,10 +201,14 @@ def wrose(dfa, speed, dir, n_bins):
                        category_orders=dirdict,
                        title="Rosa de viento: {}".format(dfa["Date"].values[0])
                        )
-    fig.show()
+    # fig.show()
+
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    fig.write_image(PATHX + name + "/wrose.png", width=1250, height=750)
 
 
-def gen_vs(stent, varx, vary):
+def gen_vs(stent, name, varx, vary):
     """ Generic VS-plot for x-vs-y arrangements.
 
     Arguments:
@@ -181,24 +218,24 @@ def gen_vs(stent, varx, vary):
     vary -- variable y
     """
     df = stent
-    df = df.sort_values(by=varx)
-    # print(df.head().to_string())
+
     fig = make_subplots(rows=1, cols=1)
 
-    fig.append_trace(go.Scatter(
-        x=df[varx],
-        y=df[vary],
-        name=(varx + " " + vary)),
-        row=1,
-        col=1
-    )
+    X = df[varx].values.reshape(-1,1)
 
-    fig.update_layout(
-        title_text=(varx + " y " + vary),
-        title_x=0.5,
-        font=dict(size=18))
+    model = LinearRegression()
+    model.fit(X, df[vary])
 
-    fig.show()
+    x_range = np.linspace(X.min(), X.max(), 100)
+    y_range = model.predict(x_range.reshape(-1, 1))
+
+    fig = px.scatter(df, x=varx, y=vary, opacity=0.65)
+    fig.add_traces(go.Scatter(x=x_range, y=y_range, name='Regression Fit'))
+
+    # fig.show()
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    fig.write_image(PATHX + name + "/gen_vs.png", width=1250, height=750)
 
 
 def sep_df(df, column):
@@ -215,7 +252,7 @@ def sep_df(df, column):
     return frames
 
 
-def box_plot(df, date, *argv):
+def box_plot(df, name, date, *argv):
     df = grpday_df(df)
     fig = make_subplots(rows=1, cols=1)
     for arg in argv:
@@ -228,7 +265,11 @@ def box_plot(df, date, *argv):
                       xaxis_title_text='día',  # yaxis label
                       bargap=0.0  # gap between bars of the same location coordinates
                       )
-    fig.show()
+    # fig.show()
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    fig.write_image(PATHX + name + "/box_plot_" + argv[0] +".png", width=1250, height=750)
+
 
 
 def grpday_df(df):
@@ -242,10 +283,10 @@ def flipdir(df, dir):
     return df
 
 
-def avg_plot(df, date, *argv):
+def avg_plot(df, name, date, *argv):
     dfa = df.copy(deep=True)
-    dfa = dfa.resample('D', on=date).mean()
-    print(dfa)
+    dfa = dfa.resample('H', on=date).mean()
+    # print(dfa)
     fig = make_subplots(rows=1, cols=1)
     for arg in argv:
         fig.append_trace(go.Scatter(x=dfa.index, y=dfa[arg], name=arg), row=1, col=1)
@@ -257,4 +298,58 @@ def avg_plot(df, date, *argv):
                       xaxis_title_text='día',  # yaxis label
                       bargap=0.0  # gap between bars of the same location coordinates
                       )
+    # fig.show()
+
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    fig.write_image(PATHX + name + "/avg_plot.png", width=1250, height=750)
+
+
+def heat(df, name):
+    matrix = np.triu(df.corr())
+    sns.heatmap(df.corr(), annot=True, fmt='.1g', mask=matrix)
+    # plt.show()
+
+    if not os.path.exists(PATHX + name + "/"):
+        os.makedirs(PATHX + name + "/")
+    plt.tight_layout()
+    plt.savefig(PATHX + name + "/heatmap.png")
+    plt.clf()
+
+
+def compare(dfs, stmo, endmo, arg):
+    fig = make_subplots(rows=1, cols=1)
+    i = 0
+    for df in dfs:
+        dfa = df.resample('D', on="Date").mean()
+        i += 1
+        # date adjustment
+        # df = df_tf(df, dfa.index, 2019, stmo, 1, 2019, endmo, 1)
+        # df = df.sort_values(by=[dfa.index])
+        # df = remove_outliers(df, var1)
+
+        fig.append_trace(go.Scatter(
+            x=dfa.index,
+            y=dfa[arg],
+            name="{0}, Turbina {1}".format(arg,i)),
+            row=1,
+            col=1
+        )
+
+
+    fig.update_layout(
+        title_text=(" "),
+        title_x=0.5,
+        font=dict(size=18))
+
     fig.show()
+    # if not os.path.exists(PATHX + name + "/"):
+    #     os.makedirs(PATHX + name + "/")
+    # fig.write_image(PATHX + name + "/avg_plot" + argv[0] + ".png", width=1250, height=750)
+    # return 0
+
+
+# dfa = df.copy(deep=True)
+# dfa = dfa.resample('D', on="Date").mean()
+# dfa = dfa.sort_values(by=varx)
+# # print(df.head().to_string())
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-from funcs import to_df, info, gen_scat, wrose, sep_df, box_plot, avg_plot
-
+from funcs import to_df, info, gen_scat, wrose, sep_df, box_plot, avg_plot, gen_vs, heat, compare
 
 # Main sequence
-data = to_df("PARAMETROS_PROTOTIPO(WIP10+ 2019).xlsx")
-info(data, ' WIND_SPEED', ' ROTOR_SPEED', ' V_OUT')
-gen_scat(data, '06', '11', 'Date', ' ROTOR_SPEED', ' WIND_SPEED', ' V_OUT')
-#
-# # gen_vs(data, ' WIND_SPEED', ' ROTOR_SPEED')
-box_plot(data, "Date", ' WIND_SPEED', ' V_OUT')
-avg_plot(data, "Date", ' WIND_SPEED', ' V_OUT', )
-wrose(data, ' WIND_SPEED', ' WIND_DIRECTION', 4)
-frame_month = sep_df(data, "Date")
-for x in frame_month:
-    wrose(x, ' WIND_SPEED', ' WIND_DIRECTION', 4)
+sheet_t1 = 'TURBINA-1'
+df_t1 = to_df("PARAMETROS_PROTOTIPO(WIP10+ 2019).xlsx", sheet_t1)
+# info(df_t1, ' WIND_SPEED', ' ROTOR_SPEED', ' V_OUT')
+# gen_scat(df_t1, sheet_t1, '06', '11', 'Date', ' V_OUT')
+# gen_scat(df_t1, sheet_t1, '06', '11', 'Date', ' WIND_SPEED')
+# gen_scat(df_t1, sheet_t1, '06', '11', 'Date', ' ROTOR_SPEED')
+# heat(df_t1[[' WIND_SPEED', ' ROTOR_SPEED', ' V_OUT', ' GRID_POWER', ' POWER_HZ']], sheet_t1)
+# gen_vs(df_t1, sheet_t1,' ROTOR_SPEED', ' V_OUT')
+# # #
+# gen_vs(df_t1, sheet_t1,  ' ROTOR_SPEED', ' V_OUT')
+# box_plot(df_t1, sheet_t1, "Date", ' WIND_SPEED')
+# box_plot(df_t1, sheet_t1, "Date",' V_OUT')
+# avg_plot(df_t1, sheet_t1, "Date", ' WIND_SPEED', ' ROTOR_SPEED')
+# wrose(df_t1, sheet_t1, ' WIND_SPEED', ' WIND_DIRECTION', 6)
+# frame_month = sep_df(df_t1, "Date")
+# for x in frame_month:
+#     wrose(x, sheet_t1, ' WIND_SPEED', ' WIND_DIRECTION', 6)
+
+# Second sheet
+sheet_t2 = 'TURBINA-2'
+df_t2 = to_df("PARAMETROS_PROTOTIPO(WIP10+ 2019).xlsx", sheet_t2)
+# info(df_t2, ' WIND_SPEED', ' ROTOR_SPEED', ' V_OUT')
+# gen_scat(df_t2, sheet_t2, '06', '11', 'Date', ' V_OUT')
+# gen_scat(df_t2, sheet_t2, '06', '11', 'Date', ' WIND_SPEED')
+# gen_scat(df_t2, sheet_t2, '06', '11', 'Date', ' ROTOR_SPEED')
+# heat(df_t2[[' WIND_SPEED', ' ROTOR_SPEED', ' V_OUT', ' GRID_POWER', ' POWER_HZ']], sheet_t2)
+# gen_vs(df_t2, sheet_t2,' ROTOR_SPEED', ' V_OUT')
+# # #
+# gen_vs(df_t2, sheet_t2,  ' ROTOR_SPEED', ' V_OUT')
+# box_plot(df_t2, sheet_t2, "Date", ' WIND_SPEED')
+# box_plot(df_t2, sheet_t2, "Date",' V_OUT')
+# avg_plot(df_t2, sheet_t2, "Date", ' WIND_SPEED', ' ROTOR_SPEED')
+# wrose(df_t2, sheet_t2, ' WIND_SPEED', ' WIND_DIRECTION', 6)
+# frame_month = sep_df(df_t2, "Date")
+# for x in frame_month:
+#     wrose(x, sheet_t2, ' WIND_SPEED', ' WIND_DIRECTION', 6)
+
+# Comparison of two sheets
+compare((df_t1, df_t2), '06', '11', ' WIND_SPEED')
+compare((df_t1, df_t2), '06', '11', ' ROTOR_SPEED')
+compare((df_t1, df_t2), '06', '11', ' V_OUT')
